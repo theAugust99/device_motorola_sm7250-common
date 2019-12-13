7c00c5862f24b8ffa7eb9fbc23ddeab5f3dc1dfc
Mahesh Sharma <smahesh@codeaurora.org>
2019-12-13 14:35:14 -0800

    sm7250-common: ANT: Support new ANT stack
    Add appropriate package name on the basis of
    transport used on target.
    
    Change-Id: I02163a7a50afba8c6ee6a766327b78c5df081d4e

--- common.mk
+++ common.mk
@@ -112,6 +112,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService-Soong \
+    com.dsi.ant.antradio_library \
+    com.dsi.ant@1.0.vendor
+
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
