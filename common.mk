0591fb21c492ba92b810e364950bdc1f9551a045
Kyle Lin <kylelin@google.com>
2020-03-24 15:29:51 +0800

    sm7250-common: Switch to use stable power HAL
    Bug: 151905669
    Test: build
    Change-Id: I1c336e976bc96e1618bba61b3bdef860fde72c15

--- common.mk
+++ common.mk
@@ -391,7 +391,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.lito-libperfmgr
+    android.hardware.power-service.sm7250-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
