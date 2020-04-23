6bba7e25a120a0970599f31140dee5307384682d
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2020-04-23 19:36:35 +0530

    sm7250-common: display: add libdisplayconfig.qti to product packages
    Change-Id: Ic8a69bc5368f9d41c5958181ecb091e6d0d4ad05

--- common.mk
+++ common.mk
@@ -247,6 +247,7 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     gralloc.lito \
     libdisplayconfig \
+    libdisplayconfig.qti \
     libdisplayconfig.vendor \
     libqdMetaData \
     libqdMetaData.system \
