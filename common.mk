ccda7bb9977d91c1ca68a5e375a7f45ce41ba521
Davide Garberi <dade.garberi@gmail.com>
2020-09-10 03:57:15 +0200

    sm7250-common: Build libhidltransport and libhwbinder
    Change-Id: I1b4b255ce40c94e3bf2e6234b7280fc094a9270b

--- common.mk
+++ common.mk
@@ -303,7 +303,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    libhidltransport \
+    libhwbinder \
+    libhwbinder.vendor
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
