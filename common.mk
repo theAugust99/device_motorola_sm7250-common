45a6e45085da5d6b48f5658de29ef71f68a311fd
Andrew Hexen <SyberHexen@gmail.com>
2021-08-26 06:50:43 -0700

    sm7250-common: copy a2dp_audio_policy_configuration.xml
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: I1ba6c9b3786886fd3ba4a9c8d14cc20536d100ce

--- common.mk
+++ common.mk
@@ -150,6 +150,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
 
 PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
